34c82b1d584f2ae1e079beef48f446935590459b
Sebastian Walther <109655682+SmarTTYs@users.noreply.github.com>
2024-07-31 21:39:46 +0200

    refactor: explicit api

--- src/commonMain/kotlin/io/github/smarttys/dotenv/DotEnv.kt
+++ src/commonMain/kotlin/io/github/smarttys/dotenv/DotEnv.kt
@@ -15,20 +15,20 @@ internal typealias EnvMap = Map<String, String>
  * `DotEnv` instance can be configured in its `DotEnv {}` factory function using [DotEnvBuilder].
  *  For demonstration purposes or trivial usages, DotEnv [companion][DotEnv.Companion] can be used instead.
  */
-sealed class DotEnv {
+public sealed class DotEnv {
     internal abstract val envMap: EnvMap
 
     /**
      * Returns the value corresponding to the given key, or null
      * if no such key is present.
      */
-    abstract operator fun get(key: String): String?
+    public abstract operator fun get(key: String): String?
 
     /**
      * Returns the value for given [key] and transforms it with given [transform]
      * function or null if no such key is present.
      */
-    operator fun <T> get(key: String, transform: (String) -> T): T? {
+    public operator fun <T> get(key: String, transform: (String) -> T): T? {
         val value = get(key) ?: return null
         return transform(value)
     }
@@ -36,24 +36,24 @@ sealed class DotEnv {
     /**
      * Returns true if the map contains the specified [key].
      */
-    operator fun contains(key: String): Boolean = envMap.containsKey(key)
+    public operator fun contains(key: String): Boolean = envMap.containsKey(key)
 
     /**
      * Returns a new [Iterator] for all entries in this [DotEnv].
      */
-    operator fun iterator() = envMap.iterator()
+    public operator fun iterator(): Iterator<Map.Entry<String, String>> = envMap.iterator()
 
     /**
      * Returns the value for given [key], or throws an [NoSuchElementException] if no
      * such key is present.
      */
-    fun getOrThrow(key: String) = get(key) ?: throwNoSuchElementException(key)
+    public fun getOrThrow(key: String): String = get(key) ?: throwNoSuchElementException(key)
 
     /**
      * Returns the value for the given [key] if the value is present. Otherwise, returns
      * the result of the [defaultValue] function.
      */
-    inline fun getOrElse(key: String, defaultValue: () -> String) = get(key) ?: defaultValue.invoke()
+    public inline fun getOrElse(key: String, defaultValue: () -> String): String = get(key) ?: defaultValue.invoke()
 
     /**
      * Load all entries from this [DotEnv] into the system environment.
@@ -61,13 +61,13 @@ sealed class DotEnv {
      * NOTE: "System environment" refers to different objects depending on the
      * target platform (System-Properties for JVM, Environment for Native).
      */
-    fun loadIntoSystemEnvironment(overwrite: Boolean = true) = loadEnvironmentToSystem(envMap, overwrite)
+    public fun loadIntoSystemEnvironment(overwrite: Boolean = true): Unit = loadEnvironmentToSystem(envMap, overwrite)
 
-    companion object {
+    public companion object {
         private const val DEFAULT_ENV_FILE_NAME = ".env"
         internal val DEFAULT_FILE_LIST get() = listOf(DEFAULT_ENV_FILE_NAME)
 
-        val DEFAULT: DotEnv by lazy(LazyThreadSafetyMode.NONE) {
+        public val DEFAULT: DotEnv by lazy(LazyThreadSafetyMode.NONE) {
             val reader = DotEnvReader(DEFAULT_FILE_LIST, false)
             val parser = DotEnvParser(
                 lenientKeyParsing = false,
@@ -100,7 +100,7 @@ internal class DotEnvImpl(override val envMap: EnvMap) : DotEnv() {
  * @return the property value.
  * @Throws NoSuchElementException when the env doesn't contain value for the property name.
  */
-operator fun DotEnv.getValue(thisRef: Any?, prop: KProperty<*>): String {
+public operator fun DotEnv.getValue(thisRef: Any?, prop: KProperty<*>): String {
     val key = prop.name.camelToUpperSnakeCase()
     return this.getOrThrow(key)
 }
@@ -113,7 +113,7 @@ operator fun DotEnv.getValue(thisRef: Any?, prop: KProperty<*>): String {
  * @param [other] the [DotEnv] instance to combine with this one
  * @return A new [DotEnv] instance combining the current instance and the provided one
  */
-operator fun DotEnv.plus(other: DotEnv): DotEnv = DotEnvImpl(this.envMap + other.envMap)
+public operator fun DotEnv.plus(other: DotEnv): DotEnv = DotEnvImpl(this.envMap + other.envMap)
 
 /**
  * Removes all environment variables from [other] [DotEnv] to [this]
@@ -123,7 +123,7 @@ operator fun DotEnv.plus(other: DotEnv): DotEnv = DotEnvImpl(this.envMap + other
  * @param [other] the [DotEnv] instance to remove values from this one
  * @return A new [DotEnv] instance with all elements from [other] contained in [this] instance removed
  */
-operator fun DotEnv.minus(other: DotEnv): DotEnv = DotEnvImpl(this.envMap.minus(other.envMap.keys))
+public operator fun DotEnv.minus(other: DotEnv): DotEnv = DotEnvImpl(this.envMap.minus(other.envMap.keys))
 
 /**
  * Writes all entries in [this] [DotEnv] instance into a newly created file
@@ -132,7 +132,7 @@ operator fun DotEnv.minus(other: DotEnv): DotEnv = DotEnvImpl(this.envMap.minus(
  * @receiver the [DotEnv] to write environment variables from
  * @param [filePath] for the created file
  */
-fun DotEnv.write(filePath: String) = marshallAndWriteEnvToFile(envMap, filePath)
+public fun DotEnv.write(filePath: String): Unit = marshallAndWriteEnvToFile(envMap, filePath)
 
 private fun String.camelToUpperSnakeCase() = fold(StringBuilder(length shl 1)) { acc, char ->
     if (char.isUpperCase()) {
